d136d8d6b9f315bfe68f930a156b364811e3f8df
starfirst57 <GmI514TwerK>
2024-03-27 19:48:29 +0700

    second

--- src/main/java/com/starfirst/doexam/controller/MainPageController.java
+++ src/main/java/com/starfirst/doexam/controller/MainPageController.java
@@ -1,15 +1,24 @@
 package com.starfirst.doexam.controller;
 
+import com.starfirst.doexam.service.CategoryService;
+import com.starfirst.doexam.service.QuestionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class MainPageController {
+    private final CategoryService categoryService;
+    private final QuestionService questionService;
+
 
     @GetMapping("")
-    public String mainPage(){
+    public String mainPage(Model model){
+        model.addAttribute("categories", this.categoryService.getAll(""));
         return "mainPage";
     }
 }
---
+++ src/main/java/com/starfirst/doexam/service/QuestionService.java
@@ -0,0 +1,4 @@
+package com.starfirst.doexam.service;
+
+public interface QuestionService {
+}
